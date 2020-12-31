20c0b7e9f2777fb828bf5b596985121ef517823a
liwy27 <47019005+liwy27@users.noreply.github.com>
2020-12-31 21:15:21 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ class First_child(QMainWindow, Ui_Form):
         self.img_rgb = np.zeros((400, 400, 3), dtype=np.uint8)
         self.img_rgb2 = np.zeros((400, 400, 3), dtype=np.uint8)
         self.img_cmy = np.zeros((400, 400, 3), dtype=np.uint8)
+        self.img_hsv = np.zeros((400, 400, 3), dtype=np.uint8)
 
         self.r = 1
         self.g = 1
@@ -33,29 +34,40 @@ class First_child(QMainWindow, Ui_Form):
         self.c = 0
         self.m = 0
         self.y = 0
+        self.h = 0
+        self.s = 0
+        self.v = 0
 
         frame_rgb = QImage(self.img_rgb, 400, 400, QImage.Format_RGB888)
         frame_rgb2 = QImage(self.img_rgb2, 400, 400, QImage.Format_RGB888)
         frame_cmy = QImage(self.img_cmy, 400, 400, QImage.Format_RGB888)
+        frame_hsv = QImage(self.img_hsv, 400, 400, QImage.Format_RGB888)
 
         pix_rgb = QPixmap.fromImage(frame_rgb)
         pix_rgb2 = QPixmap.fromImage(frame_rgb2)
         pix_cmy = QPixmap.fromImage(frame_cmy)
+        pix_hsv = QPixmap.fromImage(frame_hsv)
 
         self.item_rgb = QGraphicsPixmapItem(pix_rgb)  # 创建像素图元
         self.item_rgb2 = QGraphicsPixmapItem(pix_rgb2)
         self.item_cmy = QGraphicsPixmapItem(pix_cmy)
+        self.item_hsv = QGraphicsPixmapItem(pix_hsv)
 
         self.scene_rgb = QGraphicsScene()  # 创建场景
         self.scene_rgb2 = QGraphicsScene()
         self.scene_cmy = QGraphicsScene()  # 创建场景
+        self.scene_hsv = QGraphicsScene()
+
         self.scene_rgb.addItem(self.item_rgb)
         self.scene_rgb2.addItem(self.item_rgb2)
         self.scene_cmy.addItem(self.item_cmy)
+        self.scene_hsv.addItem(self.item_hsv)
 
         self.graphicsView.setScene(self.scene_rgb)
         self.graphicsView_3.setScene(self.scene_rgb2)
         self.graphicsView_2.setScene(self.scene_cmy)
+        self.graphicsView_4.setScene(self.scene_hsv)
+
         self.spinBox.valueChanged.connect(self.run)
         self.spinBox_2.valueChanged.connect(self.run)
         self.spinBox_3.valueChanged.connect(self.run)
@@ -64,8 +76,11 @@ class First_child(QMainWindow, Ui_Form):
         self.G2.valueChanged.connect(self.run)
         self.B2.valueChanged.connect(self.run)
 
-        # self.doubleSpinBox.valueChanged.connect(self.run)  #   run 改成要触发的函数
+        self.Hue.valueChanged.connect(self.run)
+        self.Sat.valueChanged.connect(self.run)
+        self.Val.valueChanged.connect(self.run)
 
+        # run 为要触发的函数
         self.doubleSpinBox.valueChanged.connect(self.run)
         self.doubleSpinBox_2.valueChanged.connect(self.run)
         self.doubleSpinBox_3.valueChanged.connect(self.run)
@@ -80,42 +95,80 @@ class First_child(QMainWindow, Ui_Form):
         self.c = self.doubleSpinBox.value()
         self.m = self.doubleSpinBox_2.value()
         self.y = self.doubleSpinBox_3.value()
+        self.h = self.Hue.value()
+        self.s = self.Sat.value()
+        self.v = self.Val.value()
 
-        # 计算色坐标
-        x1, y1, _ = self.convert(self.r, self.g, self.b)
-        x2, y2, _ = self.convert(self.r2, self.g2, self.b2)
-        d = math.sqrt(math.pow((x1-x2), 2)+math.pow((y1-y2), 2))
-        d = round(d, 4)
-        self.LineEdit_X1.setText(str(x1))
-        self.LineEdit_Y1.setText(str(y1))
-        self.LineEdit_X2.setText(str(x2))
-        self.LineEdit_Y2.setText(str(y2))
-        self.LineEdit_color_difference.setText(str(d))
         self.img_rgb[:, :, 0] = self.r
         self.img_rgb[:, :, 1] = self.g
         self.img_rgb[:, :, 2] = self.b
+
         self.img_rgb2[:, :, 0] = self.r2
         self.img_rgb2[:, :, 1] = self.g2
         self.img_rgb2[:, :, 2] = self.b2
+
         self.img_cmy[:, :, 0] = (1 - self.c) * 255
         self.img_cmy[:, :, 1] = (1 - self.m) * 255
         self.img_cmy[:, :, 2] = (1 - self.y) * 255
 
+        hsv_r, hsv_g, hsv_b = self.hsv2rbg(self.h, self.s, self.v)
+
+        self.img_hsv[:, :, 0] = hsv_r * 255
+        self.img_hsv[:, :, 1] = hsv_g * 255
+        self.img_hsv[:, :, 2] = hsv_b * 255
+
+        # 计算色坐标
+        x1, y1, _ = self.convert(self.r, self.g, self.b)
+        x2, y2, _ = self.convert(self.r2, self.g2, self.b2)
+
+        x3, y3, _ = self.convert((1 - self.c), (1 - self.m), (1 - self.y))
+        x4, y4, _ = self.convert(hsv_r, hsv_g, hsv_b)
+
+        d = math.sqrt(math.pow((x1-x2), 2)+math.pow((y1-y2), 2))
+        d = round(d, 4)
+
+        self.delta.setText(str(d))
+
+        self.LineEdit_X1.setText(str(x1))
+        self.LineEdit_Y1.setText(str(y1))
+        self.LineEdit_X2.setText(str(x2))
+        self.LineEdit_Y2.setText(str(y2))
+        self.LineEdit_X3.setText(str(x3))
+        self.LineEdit_Y3.setText(str(y3))
+        self.LineEdit_X4.setText(str(x4))
+        self.LineEdit_Y4.setText(str(y4))
+
+        cv.putText(self.img_rgb2, "Chromatic aberration:", (10, 40),
+                   cv.FONT_HERSHEY_COMPLEX_SMALL,
+                   1.3,
+                   (255, 255, 255),
+                   2)
+
+        cv.putText(self.img_rgb2, str(d), (10, 90),
+                   cv.FONT_HERSHEY_COMPLEX_SMALL,
+                   1.3,
+                   (255, 255, 255),
+                   2)
+
         frame_rgb = QImage(self.img_rgb, 400, 400, QImage.Format_RGB888)
         frame_rgb2 = QImage(self.img_rgb2, 400, 400, QImage.Format_RGB888)
         frame_cmy = QImage(self.img_cmy, 400, 400, QImage.Format_RGB888)
+        frame_hsv = QImage(self.img_hsv, 400, 400, QImage.Format_RGB888)
 
         pix_rgb = QPixmap.fromImage(frame_rgb)
         pix_rgb2 = QPixmap.fromImage(frame_rgb2)
         pix_cmy = QPixmap.fromImage(frame_cmy)
+        pix_hsv = QPixmap.fromImage(frame_hsv)
 
         self.item_rgb = QGraphicsPixmapItem(pix_rgb)  # 创建像素图元
         self.item_rgb2 = QGraphicsPixmapItem(pix_rgb2)
         self.item_cmy = QGraphicsPixmapItem(pix_cmy)
+        self.item_hsv = QGraphicsPixmapItem(pix_hsv)
 
         self.scene_rgb.addItem(self.item_rgb)
         self.scene_rgb2.addItem(self.item_rgb2)
         self.scene_cmy.addItem(self.item_cmy)
+        self.scene_hsv.addItem(self.item_hsv)
 
     def convert(self, r, g, b):
         x_0 = 0.490 * r + 0.310 * g + 0.200 * b
@@ -128,6 +181,25 @@ class First_child(QMainWindow, Ui_Form):
         y = round(y, 4)
         z = round(z, 4)
         return x, y, z
+
+    def hsv2rbg(self, h, s, v):
+        h_i = h // 60
+        f = h / 60 - h_i
+        p = v * (1 - s)
+        q = v * (1 - f * s)
+        t = v * (1 - (1 - f) * s)
+        if h_i == 0:
+            return v, t, p
+        elif h_i == 1:
+            return q, v, p
+        elif h_i == 2:
+            return p, v, t
+        elif h_i == 3:
+            return p, q, v
+        elif h_i == 4:
+            return t, p, v
+        elif h_i == 5:
+            return v, p, q
 
 
 class Second_child(QMainWindow, Ui_Form_2):
@@ -148,8 +220,7 @@ class Second_child(QMainWindow, Ui_Form_2):
         self.scene_img.addItem(self.item_img)
 
         self.graphicsView.setScene(self.scene_img)
-        # self.openFileButton.clicked.connect(self.openFile)  # 没有这两个控件了
-        # self.run.clicked.connect(self.calculate)
+
         self.get_filename_path = ""
         self.pushButton.clicked.connect(self.openFile)
         self.pushButton_2.clicked.connect(self.calculate)
